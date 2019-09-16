008f37dcca4b4ae747c483cfd898851dbde530d6
malashin <malashinanton@gmail.com>
2019-09-16 13:41:17 +1000

    Trunc output file before writing to it

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func readLines(path string) ([]string, error) {
 }
 
 func writeStringArrayToFile(filename string, strArray []string, perm os.FileMode) {
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY, perm)
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, perm)
 	if err != nil {
 		log.Panic(err)
 	}
